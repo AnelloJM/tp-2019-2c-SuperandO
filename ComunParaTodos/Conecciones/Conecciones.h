f12bbaf22c1e07713a78f089bc22322ce5836f1b
AnelloJM <juan.manuel.anello@gmail.com>
2019-09-19 18:47:57 -0300

    Revivimos a ComunParaTodos

---
+++ ComunParaTodos/Conecciones/Conecciones.h
@@ -0,0 +1,20 @@
+
+#include <commons/log.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <readline/readline.h>
+#include <unistd.h>
+#include <commons/collections/list.h>
+#include <string.h>
+
+#ifndef CONECCIONES_H_
+#define CONECCIONES_H_
+
+int conectarse_a_un_servidor(char *ip, char* puerto,t_log* log);//Retorna la conexion del socket cliente
+int iniciar_servidor(char* ip, char* puerto,t_log* logger);//Retorna la conexion del socket servidor
+int esperar_cliente_con_accept(int socket_servidor, t_log* logger);//El servidor espera para aceptar un cliente
+
+#endif /* CONECCIONES_H_ */
