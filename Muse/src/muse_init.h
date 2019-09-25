a5a12187d6c9249fab280a8798d06422cf7bddf9
cruz636 <juanbarcruz96@gmail.com>
2019-09-25 10:32:00 -0300

    Update muse_init.h
    me confundí y habia des-comentado la linea 27

--- Muse/src/muse_init.h
+++ Muse/src/muse_init.h
@@ -24,6 +24,6 @@ void leerArchivoDeConfiguracion();
 void setearValores(t_config* archivoConfig);
 int muse_init(int id, char* ip, int puerto, t_log* logger);
 void crearLogger();
-void loggear_valores(void valor,void archivo_log); //archivo_log puede ser definido por defecto
+//void loggear_valores(void valor,void archivo_log); //archivo_log puede ser definido por defecto
 
 #endif
