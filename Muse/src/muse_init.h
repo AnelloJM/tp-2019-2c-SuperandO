ff5247b1a239b426bf736ba64140109b888d422a
juan_cruz <juanbarcruz96@gmail.com>
2019-09-25 09:24:04 -0300

    cambié la forma de incluir la libreria de conexiones

--- Muse/src/muse_init.h
+++ Muse/src/muse_init.h
@@ -4,7 +4,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <readline/readline.h>
-#include <Conexiones/Conexiones.h>
+#include "../../ComunParaTodos/Conexiones/Conexiones.h" //esta libreria se define asi porque está en nuestro workspace
+//#include "../../ComunParaTodos/Conexiones/Conexiones.c" // esta la sigo poniendo por mi editor, ignorenla
 #include <commons/log.h>
 #include <commons/string.h>
 #include <commons/config.h>
@@ -23,6 +24,6 @@ void leerArchivoDeConfiguracion();
 void setearValores(t_config* archivoConfig);
 int muse_init(int id, char* ip, int puerto, t_log* logger);
 void crearLogger();
-//void loggear_valores(void valor,void archivo_log); //archivo_log puede ser definido por defecto
+void loggear_valores(void valor,void archivo_log); //archivo_log puede ser definido por defecto
 
 #endif
