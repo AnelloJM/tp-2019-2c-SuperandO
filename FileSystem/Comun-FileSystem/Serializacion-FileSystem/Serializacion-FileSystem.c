37fe19416b108948807662c88f846780d12baef7
Santiago Lovelli <lovelli.santiago.9@gmail.com>
2019-10-27 19:00:38 -0300

    shared library para el manejo de estructuras y otras cosas en el FileSystem

---
+++ FileSystem/Comun-FileSystem/Serializacion-FileSystem/Serializacion-FileSystem.c
@@ -0,0 +1,13 @@
+/*
+ * Serializacion-FeliSystem.c
+ *
+ *  Created on: 27 oct. 2019
+ *      Author: utnso
+ */
+
+
+#include "../Serializacion-FileSystem/Serializacion-FileSystem.h"
+
+void pruebitaRapida(){
+	puts("prueba");
+}
---
+++ FileSystem/Comun-FileSystem/Serializacion-FileSystem/Serializacion-FileSystem.h
@@ -0,0 +1,17 @@
+/*
+ * Serializacion-FeliSystem.h
+ *
+ *  Created on: 27 oct. 2019
+ *      Author: utnso
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifndef SERIALIZACION_FELISYSTEM_SERIALIZACION_FELISYSTEM_H_
+#define SERIALIZACION_FELISYSTEM_SERIALIZACION_FELISYSTEM_H_
+
+void pruebitaRapida();
+
+#endif /* SERIALIZACION_FELISYSTEM_SERIALIZACION_FELISYSTEM_H_ */
