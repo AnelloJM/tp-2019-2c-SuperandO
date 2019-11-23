43a23f610949f07b575ab3af1526c7fb20a99216
Santiago Lovelli <lovelli.santiago.9@gmail.com>
2019-11-23 20:26:53 -0300

    fantasmas que quedaron de otro sommit

---
+++ FileSystem/Comun-FileSystem/Manejos-Comunes/Manejos-Comunes.c
@@ -0,0 +1,11 @@
+/*
+ * Manejos-Comunes.c
+ *
+ *  Created on: 23 nov. 2019
+ *      Author: utnso
+ */
+
+void pruebita(){
+	puts("caca");
+}
+
---
+++ FileSystem/Comun-FileSystem/Manejos-Comunes/Manejos-Comunes.h
@@ -0,0 +1,13 @@
+/*
+ * Manejos-Comunes.h
+ *
+ *  Created on: 23 nov. 2019
+ *      Author: utnso
+ */
+
+#ifndef MANEJOS_COMUNES_MANEJOS_COMUNES_H_
+#define MANEJOS_COMUNES_MANEJOS_COMUNES_H_
+
+void pruebita();
+
+#endif /* MANEJOS_COMUNES_MANEJOS_COMUNES_H_ */
