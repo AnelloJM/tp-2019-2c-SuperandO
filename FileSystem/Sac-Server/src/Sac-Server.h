90c1fd3d78aae63d5931772e0c36d15b9988b5da
AnelloJM <juan.manuel.anello@gmail.com>
2019-11-28 15:59:14 -0300

    Comentarios de funciones

--- FileSystem/Sac-Server/src/Sac-Server.h
+++ FileSystem/Sac-Server/src/Sac-Server.h
@@ -65,38 +65,105 @@ typedef struct {
 /////////////////////////////////
 // FUNCIONES DE INICIALIZACION //
 ////////////////////////////////
-
+/*
+* Esta funcion inicializa todas
+* las estructuras necesarias y  
+* se ocupa de formatear el disco
+*/
 void iniciar_Sac_Server();
+/*
+* Esta funcion se ocupa de asignar
+* los valores iniciales al header
+*/
 void iniciar_header();
+/*
+* Esta funcion se ocupa de 
+* colocar la cantidad pasada por 
+* parametros de bits correspondientes
+* al bitmap en 1
+*/
 void cargar_bitmap(int cantidad);
+/*
+* Esta funcion se ocupa de 
+* setear el estado de todos los
+* nodos en 0 y poner su nombre \0
+*/
 void iniciar_tabla_de_nodos();
 
 ///////////////////////////////
 // FUNCIONES ADMINISTRATIVAS //
 //////////////////////////////
 
+/*
+* Esta funcion se ocupa de
+* retornar la hora actual 
+*/
 uint64_t timestamp();
+/*
+* Esta funcion es la principal de nuestro servidor
+* se ocupa de administrar los pedidos y direccionarlos
+* correspondientemente, y responder
+*/
 void* funcionMagica(int cliente);
+/*
+* Esta funcion retorna el tamanio de
+* el archivo pasado por parametro
+*/
 uint32_t tamanio_archivo(char *archivo);
+/*
+* Esta funcion retorna el tamanio de
+* el archivo pasado por parametro en bloques
+*/
 int tamanio_archivo_en_bloques(uint32_t tamanio);
+/*
+* Esta funcion es la encargada de
+* formatear el disco
+*/
 void limbiar_bloques_de_datos();
 
 //////////////////////////
 // FUNCIONES DEL BITMAP //
 /////////////////////////
 
+/*
+* Esta funcion retorna el primer
+* espacio vacio que encuentre en el bitmap
+*/
 int buscar_espacio_en_bitmap();
+/*
+* Esta funcion se encarga de ocupar el bit
+* indicado por el indice dado en el bitmap
+*/
 void ocupar_bloque_en_bitmap(int indice);
+/*
+* Esta funcion se encarga de liberar el bit
+* indicado por el indice dado en el bitmap
+*/
 void liberar_bloque_en_bitmap(int indice);
 
 ////////////////////////////////////
 // FUNCIONES DE LA TABLA DE NODOS //
 ///////////////////////////////////
 
+/*
+* Esta funcion contesta si dado el nombre
+* pasado por parametros, existe al menos UN
+* nodo en la tabla de nodos con ese nombre
+*/
 bool existe_nodo_con_nombre(char* nombre);
+
+/*
+* Esta funcion retorna el nodo correspondiente
+* al path pasado por parametro
+*/
 uint32_t exite_path_retornando_nodo(char* path);
 char* obtener_nombre_nodo(uint32_t numero_de_nodo);
-int buscar_nodo_libre();
+/*
+* 
+* comienza en 1 ya que toma que 0 es el
+* directorio raiz
+*/
+int hallar_nodo_libre();
 t_list* hallar_padres(char* nombre_buscado);
 ptrGBloque obtener_nodo_padre(uint32_t numero_de_nodo);
 char* obtener_nombre_nodo(uint32_t numero_de_nodo);
