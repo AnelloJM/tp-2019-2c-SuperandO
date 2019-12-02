b6d11a42c0d713eecc3af82adeec502707a2c7fb
juan_cruz <juanbarcruz96@gmail.com>
2019-12-02 11:51:25 -0300

    conexiones de muse

---
+++ Muse/src/servidor-cliente/conexion_muse.h
@@ -0,0 +1,35 @@
+#ifndef CONEXION_H_
+#define CONEXION_H_
+
+#include <stdio.h>
+#include <sys/socket.h> //For Sockets
+#include <stdlib.h>
+#include <netinet/in.h> //For the AF_INET (Address Family)
+
+
+/*
+Funciones para conectar MUSE con los procesos que usan
+la libreria libmuse.h
+*/
+
+//Variables globales
+
+struct sockaddr_in serv; //This is our main socket variable.
+uint32_t fd; //This is the socket file descriptor that will be used to identify the socket
+uint32_t conn; //This is the connection file descriptor that will be used to distinguish client connections.
+char message[100] = ""; //This array will store the messages that are sent by the server
+
+
+//Funciones
+
+
+uint32_t conectarse_a_servidor(char *ip,uint32_t puerto);
+void iniciar_servidor(char *ip,uint32_t puerto,t_log * logger);
+uint32_t esperar_cliente(uint32_t socket,t_log * logger);
+void enviar_mensaje(uint32_t socket);
+void recibir_mensaje(uint32_t socket);
+
+
+
+
+#endif
