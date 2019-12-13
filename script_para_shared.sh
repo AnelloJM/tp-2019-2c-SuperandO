96c961b0c95a20fb1012ef3e1df798cdfe73e9e5
Santiago Lovelli <lovelli.santiago.9@gmail.com>
2019-12-13 20:44:47 -0300

    Compartime, compartite

--- script_para_shared.sh
+++ script_para_shared.sh
@@ -2,5 +2,5 @@
 
 cd /home/utnso/
 
-echo \ "export LD_LIBRARY_PATH="\$"LD_LIBRARY_PATH:/home/utnso/workspace/tp-2019-2c-SuperandO/ComunParaTodos/Debug:/home/utnso/workspace/tp-2019-2c-SuperandO/FileSystem/Comun-FileSystem/Debug:/home/utnso/workspace/tp-2019-2c-SuperandO/Muse2/libmuse/Debug:/home/utnso/workspace/tp-2019-2c-SuperandO/Muse2/servidor-cliente/Debug" >> .bashrc
+echo \ "export LD_LIBRARY_PATH="\$"LD_LIBRARY_PATH:/home/utnso/workspace/tp-2019-2c-SuperandO/ComunParaTodos/Debug:/home/utnso/workspace/tp-2019-2c-SuperandO/FileSystem/Comun-FileSystem/Debug:/home/utnso/workspace/tp-2019-2c-SuperandO/Muse2/libmuse/Debug:/home/utnso/workspace/tp-2019-2c-SuperandO/Muse2/servidor-cliente/Debug:/home/utnso/workspace/tp-2019-2c-SuperandO/SerializacionSuse/Debug" >> .bashrc
 
