27aaec36dbf7c2fd6f68b511cb76d5ba596b41e6
Santiago Lovelli <lovelli.santiago.9@gmail.com>
2019-12-13 17:36:41 -0300

    script_para_shared esta shareando el skrip

---
+++ script_para_shared.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd /home/utnso/
+
+echo \ "export LD_LIBRARY_PATH="\$"LD_LIBRARY_PATH:/home/utnso/workspace/tp-2019-2c-SuperandO/ComunParaTodos/Debug:/home/utnso/workspace/tp-2019-2c-SuperandO/FileSystem/Comun-FileSystem/Debug:/home/utnso/workspace/tp-2019-2c-SuperandO/Muse2/libmuse/Debug:/home/utnso/workspace/tp-2019-2c-SuperandO/Muse2/servidor-cliente/Debug" >> .bashrc
+
