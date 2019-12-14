d3b82515b11273b2144bc2b8444f19da17bda453
Santiago Lovelli <lovelli.santiago.9@gmail.com>
2019-12-14 20:04:39 -0300

    Soraka

--- Muse3/ProgramaDePrueba/src/Prueba.c
+++ Muse3/ProgramaDePrueba/src/Prueba.c
@@ -7,13 +7,15 @@
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-//
-//#include <stdio.h>
-//#include <stdlib.h>
-//#include <libmuse/libmuse.h>
-//
-//int main(void) {
-//	puts("Superand0 Sac-Server Iniciando..."); /* prints Superand0 Sac-Server Iniciando... */
-//	prueba_de_creacion();
-//	return EXIT_SUCCESS;
-//}
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <libmuse.h>
+
+int main(void) {
+	puts("Superand0 Sac-Server Iniciando..."); /* prints Superand0 Sac-Server Iniciando... */
+	muse_init(0,"127.0.0.1",5555);
+	uint32_t tam = muse_alloc(70);
+	printf("tam: %i", tam);
+	return EXIT_SUCCESS;
+}
