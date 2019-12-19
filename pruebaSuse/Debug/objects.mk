33b83ccb85e4aa15f3e10021ceb32c321f6ea069
fedeMacias <fede-macias@hotmail.com>
2019-12-19 15:55:05 -0300

    pruebas de semaforos

--- pruebaSuse/Debug/objects.mk
+++ pruebaSuse/Debug/objects.mk
@@ -4,5 +4,5 @@
 
 USER_OBJS :=
 
-LIBS := -lcommons -lpthread -lSerializacionSuse -lComunParaTodos
+LIBS := -lcommons -lpthread -lHilolay -lSerializacionSuse -lComunParaTodos
 
--- pruebaSuse/src/pruebaDeSuse.c
+++ pruebaSuse/src/pruebaDeSuse.c
@@ -26,11 +26,11 @@ int main(void) {
 	log_info(hilolay_logger, "Me conectare al puerto: %s", server_port);
 	hilolay_t *threadCreate =malloc(sizeof(hilolay_t));
 	hilolay_create(threadCreate,NULL,(void*)f1,NULL);
-	//hilolay_sem_t* semaforoPrueba;
-	//semaforoPrueba = hilolay_sem_open(strdup("A"));
-	//hilolay_wait(semaforoPrueba);
-	//hilolay_signal(semaforoPrueba);
-	hilolay_join(threadCreate);
-	//int respuesta = hilolay_return(0);  //En las pruebas esta como return hilolay_return(0);
+	hilolay_sem_t* semaforoPrueba;
+	semaforoPrueba = hilolay_sem_open(strdup("solo_hiper_mega_piola"));
+	hilolay_wait(semaforoPrueba);
+	sleep(60);
+	hilolay_signal(semaforoPrueba);
+	log_info(hilolay_logger, "LISTO, TERMINE");
 	return EXIT_SUCCESS;
 }
